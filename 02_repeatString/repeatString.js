3ca3bcdcce0a9b1947a791d48847cddcf403eb9b
MicioCodes <miciocodes@gmail.com>
2022-07-26 16:52:50 +0200

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+let str;
+let num;
 
-};
+function repeatString(str, num){
+
+    if(num > 0){
+        return str.repeat(num);
+    }
+    else
+    return "";
+}
 
 // Do not edit below this line
 module.exports = repeatString;
