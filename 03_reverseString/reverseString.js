61a90809781f9cb4f332f8db4063beac4717f973
MicioCodes <miciocodes@gmail.com>
2022-07-29 04:49:17 +0200

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+let str;
 
-};
+function reverseString(str){
+
+    let splitStr = str.split("");
+    let reverseStr = splitStr.reverse();
+    let joinStr = reverseStr.join("")
+
+    return joinStr;
+
+}
 
 // Do not edit below this line
 module.exports = reverseString;
